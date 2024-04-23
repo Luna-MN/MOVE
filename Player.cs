e28c7fd78325d71ad515d0de1c73b020d18e1bd0
Matthew Nock <thematt.nock@gmail.com>
2024-04-23 15:19:14 +0100

    move

--- Player.cs
+++ Player.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Player : MeshInstance2D
+public partial class Player : RigidBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,35 +13,16 @@ public partial class Player : MeshInstance2D
 
 	public override void _Process(Double delta)
 	{
-		Vector2 velocity = new Vector2(); // Start with a vector that is not moving
-
-		// Check for input and adjust the velocity vector accordingly
-		if (Input.IsActionPressed("ui_right"))
-		{
-			velocity.X += 1;
-		}
-
-		if (Input.IsActionPressed("ui_left"))
-		{
-			velocity.X -= 1;
-		}
-
-		if (Input.IsActionPressed("ui_down"))
-		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("ui_up"))
-		{
-			velocity.Y -= 1;
-		}
-
-		// If the velocity vector is not zero, normalize it and multiply by the speed to get the final velocity
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * speed;
-		}
 
 	}
-	
+    public override void _Input(InputEvent @event) {
+		if (@event is InputEventKey keyEvent && keyEvent.Pressed) {
+			if (Input.IsKeyPressed(Key.A)) {
+				LinearVelocity = new Vector2(-speed, 0);
+			}
+			else if (Input.IsKeyPressed(Key.D)) {
+				LinearVelocity = new Vector2(-speed, 0);
+			}
+		}
+	}
 }
